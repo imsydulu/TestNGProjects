1a55543388ce5d0b59d3ffc6a2755990ee878d32
imsydulu <tsydulu9@gmail.com>
2025-03-08 01:03:27 +0530

    Assertios added

---
+++ src/test/java/com/asserts/AssertionDemo.java
@@ -0,0 +1,64 @@
+package com.asserts;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import com.utils.AppUtils;
+
+public class AssertionDemo extends AppUtils {
+	Actions action;
+	// LoginTest with valid credentials
+
+	@Parameters(value = { "username", "password" })
+	@Test
+	public void loginTest(String username, String password) {
+		action = new Actions(driver);
+		// action.scrollToElement(driver.findElement(By.xpath("//button[text()='Login']"))).perform();
+		action.scrollByAmount(0, 500).perform();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.findElement(By.name("username")).sendKeys(username);
+		driver.findElement(By.name("password")).sendKeys(password);
+		driver.findElement(By.xpath("//button[text()='Login']")).click();
+		String act = driver.findElement(By.xpath("//div[@id='flash']/b")).getText();
+		String exp = "You logged into a secure area!";
+
+		/*
+		 * boolean flag =
+		 * driver.findElement(By.xpath("//i[text()=' Logout']")).isDisplayed();
+		 * Assert.assertTrue(flag);
+		 */
+		action.scrollByAmount(0, 500).perform();
+		driver.findElement(By.xpath("//i[text()=' Logout']")).click();
+		driver.navigate().refresh();
+		Assert.assertEquals(act, exp, "Test is passed");
+		System.out.println("hard assertion");
+	}
+
+	// Softassertion
+	@Parameters(value = { "username", "password" })
+	@Test(testName = "SoftAssertionTest")
+	public void loginTestwithInvalid(String username, String password) {
+		action = new Actions(driver);
+		// action.scrollToElement(driver.findElement(By.xpath("//button[text()='Login']"))).perform();
+		action.scrollByAmount(0, 500).perform();
+
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.findElement(By.name("username")).sendKeys(username);
+		driver.findElement(By.name("password")).sendKeys(password);
+		driver.findElement(By.xpath("//button[text()='Login']")).click();
+		String act = driver.findElement(By.xpath("//div[@id='flash']/b")).getText();
+		String exp = "You logged into a secure area!";
+		SoftAssert softAssert = new SoftAssert();
+		softAssert.assertEquals(act, exp);
+		System.out.println("soft assertion");
+		softAssert.assertAll();
+		System.out.println("soft assertion");
+	}
+
+}
---
+++ src/test/java/com/gmailtest/GoogleTest.java
@@ -0,0 +1,71 @@
+package com.gmailtest;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.util.Calendar;
+import java.util.Date;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.io.FileHandler;
+import org.testng.ITestContext;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.utils.AppUtils;
+
+public class GoogleTest extends AppUtils {
+
+	@Parameters(value = { "username" })
+	@Test(testName = "GmailLogin")
+	public void gmailLoginTest(ITestContext context, String userName) throws Exception {
+		driver.findElement(By.xpath("//a[text()='Gmail']")).click();
+		// path = //div[@class='header__aside']/a/span[text()='Sign in']
+
+		driver.findElement(By.xpath("//div[@class='header__aside']/a/span[text()='Sign in']")).click();
+		driver.findElement(By.xpath("//input[@id='identifierId']")).sendKeys(userName);
+		driver.findElement(By.xpath("//div[@class='O1Slxf']/div[1]/div/div/button")).click();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(39));
+
+		if (driver.findElement(By.xpath("//a[@aria-label='Try again']")).isDisplayed()) {
+			System.out.println("Try agian is displayed-------------");
+			getScreeshot(context.getName(), timestamp("ddMMyyyyhhmmss"));
+		}
+
+	}
+
+	public String timestamp(String dateFormat) {
+		Calendar cal = Calendar.getInstance();
+		Date date = cal.getTime();
+		SimpleDateFormat fomratter = new SimpleDateFormat(dateFormat);
+		String reqDateFormat = fomratter.format(date);
+		System.out.println(reqDateFormat);
+		return reqDateFormat;
+	}
+
+	public void getScreeshot(String testname, String timestamp) throws IOException {
+		String filename = testname.concat(timestamp);
+		System.out.println("Filename " + filename);
+		TakesScreenshot screenshot = (TakesScreenshot) driver;
+		File source_file = screenshot.getScreenshotAs(OutputType.FILE);
+		boolean isCreatedDir = new File("./Screenshots").mkdirs();
+		File dest_file = new File("./Screenshots/" + filename + ".jpeg");
+		if (isCreatedDir || new File("./Screenshots").exists()) {
+			System.out.println("Saved File Name: " + dest_file.getName());
+			FileHandler.copy(source_file, dest_file);
+		} else {
+			System.out.println("File is not created");
+		}
+	}
+
+	@Test(enabled = false, priority = 1, testName = "facebooktest")
+	public void facebooktest() {
+
+		System.out.println("Nothing to execute");
+		// driver.findElement()
+	}
+
+}
